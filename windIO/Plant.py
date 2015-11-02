80d8a56cde872cd0cabb3733dac42cbde5b75b8e
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-11-02 11:53:43 +0100

    adding some tests

--- windIO/Plant.py
+++ windIO/Plant.py
@@ -14,6 +14,7 @@ class WTLayout(object):
         self.wt_names = list(data['turbines'].keys())
         self.wt_names.sort()
         self.data = data
+        self.check_structure(data)
 
     def __getitem__(self, a):
         return self.data[a]
@@ -28,3 +29,22 @@ class WTLayout(object):
     @property
     def positions(self):
         return np.array([wt['position'] for wt in self.wt_list])
+
+    def check_structure(self, data):
+        base = ['turbines', 'turbine_types']
+        for k, v in data.items():
+            if not k in base:
+                raise KeyError('key: {0} not recognized'.format(k))
+
+        # Check for all turbines to have a defined type
+        for k, v in data['turbines'].items():
+            if 'type' in v:
+                if not v['type'] in data['turbine_types']:
+                    raise Exception("turbine: {0} type: {1} is not defined in `turbine_types`".format(k, v['type']))
+            else:
+                raise KeyError("turbine: {0} doesn't have a defined type".format(k))
+
+        # Check that the turbines have a position
+        for k, v in data['turbines'].items():
+            if 'position' not in v:
+                raise Exception("turbine: {0} doesn't have a defined position".format(k))
--- windIO/test/test_wind_plant.py
+++ windIO/test/test_wind_plant.py
@@ -1,34 +1,57 @@
 import os
 from windIO.Plant import WTLayout
 import numpy as np
+import pytest
 
 # The test directory
 current_dir = os.path.dirname(__file__)
 
 class TestWindTurbineLayout:
-    def test_read_yml(self):
-        filename = current_dir + '/tplant1.yml'
-        wt_layout = WTLayout(filename)
+    def setup_method(self, method):
+        self.filename = current_dir + '/tplant1.yml'
+        self.wt_layout = WTLayout(self.filename)
 
+    def test_read_yml(self):
         # wt_name is there and correct
-        assert hasattr(wt_layout, 'wt_names')
-        assert wt_layout.wt_names == ['t1', 't2', 't3', 't4']
+        assert hasattr(self.wt_layout, 'wt_names')
+        assert self.wt_layout.wt_names == ['t1', 't2', 't3', 't4']
 
         # Get item works
-        assert wt_layout['turbines']['t1']['name'] == 'T1'
+        assert self.wt_layout['turbines']['t1']['name'] == 'T1'
 
         # Set item works
-        wt_layout['turbines']['t1']['name'] = 'T0'
-        assert wt_layout['turbines']['t1']['name'] == 'T0'
+        self.wt_layout['turbines']['t1']['name'] = 'T0'
+        assert self.wt_layout['turbines']['t1']['name'] == 'T0'
 
         # Get item is the same as data
-        assert wt_layout['turbines']['t1']['name'] == wt_layout.data['turbines']['t1']['name']
+        assert self.wt_layout['turbines']['t1']['name'] == self.wt_layout.data['turbines']['t1']['name']
 
         # Test the positions property
         np.testing.assert_array_almost_equal(
-            wt_layout.positions,
+            self.wt_layout.positions,
             np.array([[0.0, 0.0],
                       [1000.0, 0.0],
                       [1000.0, 1000.0],
                       [0.0, 1000.0],
                       ]))
+
+    def test_check_base_structure(self):
+        data = self.wt_layout.data
+        data['wrong_key'] = ''
+        with pytest.raises(KeyError) as keyinfo:
+            self.wt_layout.check_structure(data)
+        assert 'not recognized' in str(keyinfo.value)
+
+    def test_check_turbine_type_definition(self):
+        data = self.wt_layout.data
+        data['turbines']['t1']['type'] = 'Type2'
+        with pytest.raises(Exception) as keyinfo:
+            self.wt_layout.check_structure(data)
+        assert 'turbine: t1 type: Type2 is not defined in `turbine_types`' in str(keyinfo.value)
+
+    def test_check_turbine_position_definition(self):
+        data = self.wt_layout.data
+        data['turbines']['t1'].pop('position', None)
+        with pytest.raises(Exception) as keyinfo:
+            self.wt_layout.check_structure(data)
+        assert "turbine: t1 doesn't have a defined position" in str(keyinfo.value)
